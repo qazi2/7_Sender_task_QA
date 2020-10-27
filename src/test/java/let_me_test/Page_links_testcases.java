5bc05648519135b09b9908f2cbbb9c4f827a2fff
qazi <qazimujahid3@gmail.com>
2020-10-27 20:15:16 +0500

    code updated

--- src/test/java/let_me_test/Page_links_testcases.java
+++ src/test/java/let_me_test/Page_links_testcases.java
@@ -30,6 +30,48 @@ public class Page_links_testcases extends base{
     }
 
     @Test
+    public void Page_link_validation_Top_links()
+    {
+        // creating webdriver instance
+
+        System.setProperty("webdriver.chrome.driver", "/Users/qazi/Documents/selenium-java-3.141.59/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://komoot.com");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        driver.findElement(By.xpath("//*[@id='gdpr_banner_portal']/div/div/div/div/div[2]/div/div[1]/button/div/div")).click();
+
+        Page_links p = new Page_links(driver);
+        System.out.println(p.size_footer().getSize());
+        WebElement coloumndriver=p.footerdriver().findElement(By.xpath("//*[@id=\'pageMountNode']/div/div[1]/div/div/div[1]"));
+        System.out.println(coloumndriver.findElements(By.tagName("a")).size());
+
+        // clicks on each link in the coloumn and check if the pages are opening-
+        for(int i=1;i<coloumndriver.findElements(By.tagName("a")).size();i++)
+        {
+
+            String clickonlinkTab=Keys.chord(Keys.COMMAND,Keys.ENTER);
+
+            coloumndriver.findElements(By.tagName("a")).get(i).sendKeys(clickonlinkTab);
+
+
+        }
+        // opens all the tabs
+        Set<String> abc=driver.getWindowHandles();//4
+        Iterator<String> it=abc.iterator();
+        // coming back to all tabs and capturing the title of page
+        while(it.hasNext())
+        {
+
+            driver.switchTo().window(it.next());
+            System.out.println(driver.getTitle());
+
+        }
+
+
+    }
+    @Test
     public void Page_link_validation_bottom_links()
     {
         // Navigating to a webpage
@@ -69,54 +111,13 @@ public class Page_links_testcases extends base{
     }
 
 
-    @Test
-    public void Page_link_validation_Top_links()
-    {
-        // creating webdriver instance
 
-		System.setProperty("webdriver.chrome.driver", "/Users/qazi/Documents/selenium-java-3.141.59/chromedriver");
-        WebDriver driver = new ChromeDriver();
-
-		driver.get("http://komoot.com");
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-		driver.findElement(By.xpath("//*[@id='gdpr_banner_portal']/div/div/div/div/div[2]/div/div[1]/button/div/div")).click();
-
-        Page_links p = new Page_links(driver);
-        System.out.println(p.size_footer().getSize());
-        WebElement coloumndriver=p.footerdriver().findElement(By.xpath("//*[@id=\'pageMountNode']/div/div[1]/div/div/div[1]"));
-		System.out.println(coloumndriver.findElements(By.tagName("a")).size());
-
-        // clicks on each link in the coloumn and check if the pages are opening-
-		for(int i=1;i<coloumndriver.findElements(By.tagName("a")).size();i++)
-    {
-
-        String clickonlinkTab=Keys.chord(Keys.COMMAND,Keys.ENTER);
-
-        coloumndriver.findElements(By.tagName("a")).get(i).sendKeys(clickonlinkTab);
-
-
-    }
-		// opens all the tabs
-    Set<String> abc=driver.getWindowHandles();//4
-    Iterator<String> it=abc.iterator();
-        // coming back to all tabs and capturing the title of page
-		while(it.hasNext())
-    {
-
-        driver.switchTo().window(it.next());
-        System.out.println(driver.getTitle());
-
-    }
-
-
-}
 
     @AfterTest
     public void teardown()
     {
 
-        driver.close();
+        driver.quit();
 
     }
 
